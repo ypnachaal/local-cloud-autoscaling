84073f28448f520147a3dc302f80cdc6117923b2
ypnachaal <m25ai2111@iitj.ac.in>
2026-03-29 20:04:28 +0530

    Refactor VM creation script to use external startup script
    Updated the startup script for GCP VM creation to use an external file instead of inline script.

--- create_cloud_vm.sh
+++ create_cloud_vm.sh
@@ -1,23 +1,7 @@
-cat > create_cloud_vm.sh << 'EOF'
 gcloud compute instances create autoscale-vm-1 \
---zone=us-central1-a \
---machine-type=e2-micro \
---image-family=ubuntu-2204-lts \
---image-project=ubuntu-os-cloud \
---tags=http-server \
---metadata=startup-script='#! /bin/bash
-sudo apt update
-sudo apt install -y python3-pip
-pip3 install flask
-cat <<APP > app.py
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from GCP VM"
-
-app.run(host="0.0.0.0", port=80)
-APP
-python3 app.py'
-EOF
+  --zone=us-central1-a \
+  --machine-type=e2-micro \
+  --image-family=ubuntu-2204-lts \
+  --image-project=ubuntu-os-cloud \
+  --tags=http-server \
+  --metadata-from-file startup-script=createVm.sh
